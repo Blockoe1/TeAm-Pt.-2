54abb831ca4e06af8b5d226cf4cf750375486795
Cade Naylor <arnaylor@mail.bradley.edu>
2026-02-27 23:10:41 -0600

    Mian menu exists
    Start doesnt work; the other buttons do

---
+++ Assets/Scripts/UIControls.cs
@@ -0,0 +1,50 @@
+/* Author Name:     Cade Naylor           
+ * Created Date:    2/27/2026
+ * Modified Date:   2/27/2026
+ * Description:     Controls UI functionality
+ * */
+using UnityEditor;
+using UnityEngine;
+
+public class UIControls : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainCanvas;
+    [SerializeField] private GameObject _credits;
+    [SerializeField] private GameObject _options;
+
+
+    private void Start()
+    {
+        _mainCanvas.SetActive(true);
+        _credits.SetActive(false);
+        _options.SetActive(false);
+    }
+
+    public void CreditClick()
+    {
+        _credits.SetActive(true);
+        _mainCanvas.SetActive(false);
+    }
+
+    public void OptionsClick()
+    {
+        _options.SetActive(true);
+        _mainCanvas.SetActive(false);
+    }
+
+    public void Back()
+    {
+        _credits.SetActive(false);
+        _options.SetActive(false);
+        _mainCanvas.SetActive(true);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+
+    }
+}
