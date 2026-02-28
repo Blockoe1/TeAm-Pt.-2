556a71b0e822aae1b5433711e97f96ac1a455a9e
Super-Fun-Inc <159081917+EvanDalyGameDesign@users.noreply.github.com>
2026-02-28 11:31:35 -0600

    FryingPan

---
+++ Assets/Scripts/Bosses/FryingPan/FP_FireButter.cs
@@ -0,0 +1,28 @@
+using NaughtyAttributes;
+using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+[DropdownGroup("FryingPan")]
+public class FP_FireButter : BossAction
+{
+    [SerializeField] protected ProjectileShooter shooter;
+    [SerializeField] protected float shotPower;
+    [SerializeField] private int shotCount;
+    [SerializeField] protected int spread;
+    [SerializeField, ShowIf("ShowShotDelay"), AllowNesting] private float shotDelay;
+    [SerializeField] private float postShotWait;
+
+    private bool ShowShotDelay => shotCount > 1;
+    public override IEnumerator ActionRoutine()
+    {
+        for (int i = 0; i < shotCount; i++)
+        {
+            shooter.Shoot(Boss.ToPlayer, shotPower, shotCount, spread);
+            yield return new WaitForSeconds(shotDelay);
+        }
+
+        yield return new WaitForSeconds(postShotWait);
+        Phase.NextAction();
+    }
+}
