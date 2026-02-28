e75ee1a6543c8f79e1bcc51f0a0e1f39bf8d025c
IceyFox006 <cgreenan@mail.bradley.edu>
2026-02-27 20:43:10 -0600

    RollSlide

--- Assets/Scripts/PMoveRollSt.cs
+++ Assets/Scripts/PMoveRollSt.cs
@@ -29,13 +29,16 @@ public class PMoveRollSt : PMoveBaseSt
 
     private void Move()
     {
-        //Vector2 targetSpeed = m.MoveDirection * m.RollSpeed;
-        //targetSpeed = new Vector2(Mathf.Lerp(m.Rb2d.linearVelocity.x, targetSpeed.x, 1), Mathf.Lerp(m.Rb2d.linearVelocity.y, targetSpeed.y, 1));
+        Vector2 targetSpeed = m.MoveDirection * m.RollSpeed;
+        targetSpeed = new Vector2(Mathf.Lerp(m.Rb2d.linearVelocity.x, targetSpeed.x, 1), Mathf.Lerp(m.Rb2d.linearVelocity.y, targetSpeed.y, 1));
 
-        //float accelRate = (Mathf.Abs(targetSpeed.x) > 0.01f) ? accelAmount : deccelAmount;
+        float accelRateX = (Mathf.Abs(targetSpeed.x) > 0.01f) ? accelAmount : deccelAmount;
+        float accelRateY = (Mathf.Abs(targetSpeed.y) > 0.01f) ? accelAmount : deccelAmount;
 
-
-        //Vector2 speedDifference = new Vector2(targetSpeed.x - m.Rb2d.linearVelocity.x, targetSpeed.y - m.Rb2d.linearVelocity.y);
-        m.Rb2d.linearVelocity = m.MoveDirection * m.RollSpeed;
+        Vector2 speedDifference = new Vector2(targetSpeed.x - m.Rb2d.linearVelocity.x, targetSpeed.y - m.Rb2d.linearVelocity.y);
+        Vector2 movement = speedDifference * new Vector2(accelRateX, accelRateY);
+        Debug.Log(movement);
+        m.Rb2d.AddForce(movement, ForceMode2D.Force);
+        //m.Rb2d.linearVelocity = m.MoveDirection * m.RollSpeed;
     }
 }
