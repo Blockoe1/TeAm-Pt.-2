e3c688c1896aaea0ff7d2b5f3f7c8fbd3c2561b8
ablumhardt04 <ablumhardt@mail.bradley.edu>
2026-02-28 18:57:39 -0600

    All fonts are in

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -12,7 +12,7 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] private List<Speaker> _speakers;
     private Dictionary<SpeakerName, Speaker> nameToSpeaker;
 
-    public enum SpeakerName { Undefined, Eggy, MixingBowl, FryingPan }
+    public enum SpeakerName { Undefined, Eggy, MixingBowl, FryingPan, HappyPan, EggCooker, HappyCooker }
 
     [Serializable]
     public class Speaker
