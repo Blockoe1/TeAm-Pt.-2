25edb1c4c0a03765d31ed510f790090cbc899edb
IceyFox006 <cgreenan@mail.bradley.edu>
2026-03-01 10:44:02 -0600

    PanHitParticle

--- Assets/Scripts/Player/PDamage.cs
+++ Assets/Scripts/Player/PDamage.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class PDamage : MonoBehaviour
 {
     [SerializeField] private int _damage;
+    [SerializeField] private bool _pan;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out BossHealth boss))
         {
             boss.Damage(_damage);
+
+            if(_pan)
+                ParticleMngr.Inst.Play("PAN_HIT", transform.position, transform.rotation);
+
             if (AudioManager.instance != null)
             {
                 AudioManager.instance.PlayOneShot(FMODEvents.instance.PlayerMeleeAttack);
