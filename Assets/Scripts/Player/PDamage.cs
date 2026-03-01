52d44e4a532a56b39e7d898fb6b7761cad78507d
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-03-01 16:09:15 -0600

    Pan Juice

--- Assets/Scripts/Player/PDamage.cs
+++ Assets/Scripts/Player/PDamage.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PDamage : MonoBehaviour
 {
     [SerializeField] private int _damage;
     [SerializeField] private bool _pan;
+    [SerializeField] private UnityEvent OnPanHit;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,7 +14,10 @@ public class PDamage : MonoBehaviour
             boss.Damage(_damage);
 
             if(_pan)
+            {
                 ParticleMngr.Inst.Play("PAN_HIT", transform.position, transform.rotation);
+                OnPanHit?.Invoke();
+            }
 
             print("Form: " + FindAnyObjectByType<PlayerHealth>().form);
             if (FindAnyObjectByType<PlayerHealth>().form == PlayerHealth.eggform.yolk)
